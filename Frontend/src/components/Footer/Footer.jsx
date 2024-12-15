08def65c7d6ec53c0b657ff553814b5d739338bb
Basit Ali <opoali911@gmail.com>
2024-12-15 13:36:50 -0800

    make footer of the shoe project

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -3,8 +3,25 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="footerbody">
+        <div className="help">
+          <h3>Help</h3>
+        </div>
+        <div className="shop">
+          <h3>Shop</h3>
+        </div>
+        <div className="company">
+          <h3>Company</h3>
+        </div>
+        <div className="allBasit">
+          <h3>All</h3>
+        </div>
+      </div>
+      <div className="footerpart">
+        <div className="icon">Icons</div>
+        <div className="copyright">Copy Right</div>
+      </div>
     </div>
   )
 }
