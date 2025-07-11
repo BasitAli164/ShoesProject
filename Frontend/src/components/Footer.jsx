736d9161e43afb820b228ba9ef4f32beea02f80a
Basit Ali <opoali911@gmail.com>
2025-07-11 21:40:58 +0500

    add more data in db

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Grid, Typography, Link, IconButton, Box } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
